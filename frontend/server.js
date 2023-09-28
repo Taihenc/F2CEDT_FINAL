512fe1a8e0cc6ad1163a73cf5acd26cb2eb122b6
Taihen <51960471+Taihenc@users.noreply.github.com>
2023-09-28 20:00:05 +0700

    remove JSDOM

--- frontend/server.js
+++ frontend/server.js
@@ -2,11 +2,9 @@ import express from 'express';
 import home from './public/scripts/home.js';
 import fs from 'fs';
 import path from 'path';
-import { JSDOM } from 'jsdom';
 
 const app = express();
 const PORT = 80;
-const dom = new JSDOM();
 
 app.use(express.static('public'));
 
@@ -23,14 +21,13 @@ app.get('/', (req, res) => {
 				console.log(err);
 				res.status(500).send('Some error happened');
 			}
-			const html = dom.window.document.createElement('html');
-			html.innerHTML = data;
-
-			// html.getElementsByTagName('main')[0].innerHTML += home(0, true);
-			html.getElementsByTagName('main')[0].innerHTML += home(1, false);
-			html.getElementsByTagName('main')[0].innerHTML += home(2, false);
-			html.getElementsByTagName('main')[0].innerHTML += home(3, false);
-			return res.send(html.outerHTML);
+			let html = data;
+			let append = '';
+			// append += home(0, true);
+			append += home(1, false);
+			append += home(2, false);
+			append += home(3, false);
+			return res.send(html.replace('<!-- replace me! -->', append));
 		}
 	);
 });
